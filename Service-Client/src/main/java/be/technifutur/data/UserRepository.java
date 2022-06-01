38dc75194db1486997eab72383eb862011fa436c
Crespin <crespin.christophe@netc.eu>
2022-06-01 11:33:12 +0200

    Ajout de l'interface UserRepository

---
+++ Service-Client/src/main/java/be/technifutur/data/UserRepository.java
@@ -0,0 +1,7 @@
+package be.technifutur.data;
+
+import be.technifutur.model.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
--- Service-Client/src/main/java/be/technifutur/metier/mapper/UserMapper.java
+++ Service-Client/src/main/java/be/technifutur/metier/mapper/UserMapper.java
@@ -1,11 +1,18 @@
 package be.technifutur.metier.mapper;
 
 
+import be.technifutur.model.entities.User;
+import be.technifutur.model.forms.UserForm;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserMapper {
 
+    public User formToEntity(UserForm form){
+
+        return
+
+    }
 
 
 }
