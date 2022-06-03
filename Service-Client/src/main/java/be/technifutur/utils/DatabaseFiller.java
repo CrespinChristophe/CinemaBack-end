d516b3011c5ca7776613b858ac05a6feb5af7cab
Crespin <crespin.christophe@netc.eu>
2022-06-03 16:58:28 +0200

    Ajout databasefiller et dernier commit de la journée

---
+++ Service-Client/src/main/java/be/technifutur/utils/DatabaseFiller.java
@@ -0,0 +1,30 @@
+package be.technifutur.utils;
+
+
+import be.technifutur.data.UserRepository;
+import be.technifutur.model.entities.User;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DatabaseFiller implements InitializingBean {
+
+    private UserRepository repository;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        setupUser();
+    }
+
+    private void setupUser() {
+
+        User u = User.builder()
+                .build()
+                .id(1L)
+                .nom("Crespin")
+                .prenom("Christophe")
+                .adresse("rue des gagnants")
+                
+
+    }
+}
