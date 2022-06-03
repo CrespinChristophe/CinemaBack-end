20dfbdcea0acc5095b2fb1dcf1e91ce5ced6e275
Crespin <crespin.christophe@netc.eu>
2022-06-03 09:58:18 +0200

    Ajout de UserMapper

--- Service-Client/src/main/java/be/technifutur/metier/mapper/UserMapper.java
+++ Service-Client/src/main/java/be/technifutur/metier/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package be.technifutur.metier.mapper;
 
 
+import be.technifutur.model.dto.UserDTO;
 import be.technifutur.model.entities.User;
 import be.technifutur.model.forms.UserForm;
 import org.springframework.stereotype.Service;
@@ -10,7 +11,34 @@ public class UserMapper {
 
     public User formToEntity(UserForm form){
 
-        return
+        if(form == null)
+            return null;
+
+        return User.builder()
+                .nom(form.getNom())
+                .prenom(form.getPrenom())
+                .adresse(form.getAdresse())
+                .dateNaissance(form.getDateNaissance())
+                .numeroTel(form.getNumeroTel())
+                .email(form.getEmail())
+                .build();
+
+    }
+
+    public UserDTO entityToDTO(User entity){
+
+        if(entity == null)
+            return null;
+
+        return UserDTO.builder()
+                .id(entity.getId())
+                .nom(entity.getNom())
+                .prenom(entity.getPrenom())
+                .adresse(entity.getAdresse())
+                .dateNaissance(entity.getDateNaissance())
+                .numeroTel(entity.getNumeroTel())
+                .email(entity.getEmail())
+                .build();
 
     }
 
